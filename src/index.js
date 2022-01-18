200b33d5c4a426641dcc699c84ee579a8338d4ac
Francois Spies <fcspies@gmail.com>
2022-01-18 12:56:50 -0500

    saved

--- src/index.js
+++ src/index.js
@@ -20,4 +20,3 @@ function addTask() {
 // As a user, I should be able to click some form of a submit button.
 
 // As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
-
